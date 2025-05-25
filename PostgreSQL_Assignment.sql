0c4338c11f4956268b44a01c9542f61c1b2f74ad
zahid-official <zahid.official8@gmail.com>
2025-05-26 00:48:37 +0600

    Sightings Tables value inserted

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -41,3 +41,9 @@ CREATE TABLE sightings (
     location VARCHAR(100) NOT NULL,
     notes TEXT
 );
+
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
