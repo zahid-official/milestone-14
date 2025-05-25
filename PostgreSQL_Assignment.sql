745c801c2cfe0f821ea4c29fde67be8e35a70323
zahid-official <zahid.official8@gmail.com>
2025-05-26 00:46:46 +0600

    Species Tables value inserted

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -25,6 +25,11 @@ CREATE TABLE species (
     conservation_status VARCHAR(50) NOT NULL
 );
 
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
+('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
+('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
+('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
+('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
 
 
