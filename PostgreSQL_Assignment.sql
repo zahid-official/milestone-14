cba5ac67e5dc0f85e5a0fe5b7e147e8b60dd0517
zahid-official <zahid.official8@gmail.com>
2025-05-26 00:24:13 +0600

    Rangers table created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,2 +1,11 @@
 -- Active: 1747811270922@@127.0.0.1@5432@conservation_db
 CREATE DATABASE conservation_db;
+
+
+
+
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    region VARCHAR(50)
+);
