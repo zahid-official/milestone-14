0f3230fe341f67c0f4e34dd3f4f18fa671a8170c
zahid-official <zahid.official8@gmail.com>
2025-05-26 00:42:28 +0600

    Adding constraints

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -6,8 +6,8 @@ CREATE DATABASE conservation_db;
 
 CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
-    name VARCHAR(50),
-    region VARCHAR(50)
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(100) NOT NULL
 );
 
 
@@ -15,10 +15,10 @@ CREATE TABLE rangers (
 
 CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
-    common_name VARCHAR(50),
-    scientific_name VARCHAR(50),
-    discovery_date DATE,
-    conservation_status VARCHAR(20)
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(50) NOT NULL
 );
 
 
